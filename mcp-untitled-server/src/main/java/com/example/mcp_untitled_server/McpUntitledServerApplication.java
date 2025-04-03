8e55d0c42d32230d6d55a1ebae903e7af00df61d
s3915185 <tran.vuhoangsf19@gmail.com>
2025-04-03 15:18:12 +0700

    add mcp-untitled-server

---
+++ mcp-untitled-server/src/main/java/com/example/mcp_untitled_server/McpUntitledServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcp_untitled_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpUntitledServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpUntitledServerApplication.class, args);
+	}
+
+}
